3163451295e03282717de707778976df6544cc44
mehdi <mehdi.ben3@hotmail.com>
2024-10-22 15:23:23 -0400

    début

---
+++ database/migrations/2024_10_04_182518_create_voitures_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoituresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voitures', function (Blueprint $table) {
+            $table->id();
+            $table->string('marque');
+            $table->string('annee');
+            $table->string('modele');
+            $table->string('valeur');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voitures');
+    }
+}
---
+++ database/migrations/2024_10_22_190828_create_offres_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offres', function (Blueprint $table) {
+            $table->id();
+            $table->string('prix');
+            $table->string('message');
+            $table->foreignId('voiture_id')
+            ->references('id')
+            ->on('voitures')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreignId('user_id')
+            ->references('id')
+            ->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offres');
+    }
+}
