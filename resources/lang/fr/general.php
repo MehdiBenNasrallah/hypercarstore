c6afd202dfae2c8c5d30e5ce58d4232c367bdc00
mehdi <mehdi.ben3@hotmail.com>
2024-11-06 08:57:30 -0500

    petite modif

--- resources/lang/fr/general.php
+++ resources/lang/fr/general.php
@@ -1,12 +1,7 @@
 <?php
 // FR folder
 return [
-    'Liste des articles' => 'Liste des articles',
     'apropos' => 'A propos',
-    'Ajouter un article' => 'Ajouter un article',
-    'Ajouter' => 'Ajouter un article',
-    'Titre' => 'Titre',
-    'Savoir' => 'En savoir plus',
-    'Monopage'=>'Monopage',
-    'Accueil'=>'Accueil',
+    
+    
 ];
